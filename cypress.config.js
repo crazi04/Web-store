dbd79680e966d16550803517b43ad8c0f1036f00
“Vita <is2bequeen@gmail.com>
2023-06-05 23:18:22 +0300

    purchasing

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    testIsolation: false,
   },
 });
